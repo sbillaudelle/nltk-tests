aabe6ee5671e4fd0491105bfbba2499609ca315d
Sebastian Billaudelle <stein@cream-project.org>
2010-05-19 14:53:18 +0200

    Improved version.

--- tagging.py
+++ tagging.py
@@ -3,16 +3,14 @@ import nltk.tag
 import nltk.tokenize
 
 TRAIN_DATA = [
-    [('fuck', 'ACTION'), ('meeting', 'FOO'), ('aaa', 'BAR')],
-    [('add', 'ACTION'), ('meeting', 'aaa')],
+    [('do', 'ACTION_DO'), ('meeting', 'FOO')],
+    [('add', 'ACTION_ADD'), ('meeting', 'DATE_TYPE')],
     ]
-#DATA = ["add", "23:00"]
-#DATA = ["fuck", "meeting", "aaa"]
-DATA = ["add", "meeting"]
 
 re_patterns = [('..:..', 'TIME')]
 re_tagger = nltk.tag.RegexpTagger(re_patterns)
 
-tagger = nltk.tag.BigramTagger(train=TRAIN_DATA, backoff=re_tagger)
+tagger = nltk.tag.TrigramTagger(train=TRAIN_DATA, backoff=re_tagger)
 
-print tagger.tag(DATA)
+print tagger.tag(["add", "meeting", "at", "20:00"])
+print tagger.tag(["do", "meeting"])
