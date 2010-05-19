e5c11b9e3bdcaa4f5075d92f33b4047f41af35e8
Sebastian Billaudelle <stein@cream-project.org>
2010-05-19 15:01:07 +0200

    Using brown corpus.

--- tagging.py
+++ tagging.py
@@ -1,16 +1,22 @@
 import nltk
 import nltk.tag
 import nltk.tokenize
+import nltk.corpus
 
 TRAIN_DATA = [
     [('do', 'ACTION_DO'), ('meeting', 'FOO')],
     [('add', 'ACTION_ADD'), ('meeting', 'DATE_TYPE')],
     ]
 
+brown_train = nltk.corpus.brown.tagged_sents()
+
 re_patterns = [('..:..', 'TIME')]
 re_tagger = nltk.tag.RegexpTagger(re_patterns)
 
-tagger = nltk.tag.TrigramTagger(train=TRAIN_DATA, backoff=re_tagger)
+brown_tagger = nltk.tag.TrigramTagger(train=brown_train, backoff=re_tagger)
+
+tagger = nltk.tag.TrigramTagger(train=TRAIN_DATA, backoff=brown_tagger)
 
 print tagger.tag(["add", "meeting", "at", "20:00"])
 print tagger.tag(["do", "meeting"])
+print tagger.tag(['The', 'dog', 'is', 'yellow'])
